dbcb96021321ccb5687c4ddf2dc5c5740d919af8
Vriyas Hartama <vriyashartama@gmail.com>
2018-12-22 11:42:06 +0700

    Remove comment

--- admin/sql/init_database.sql
+++ admin/sql/init_database.sql
@@ -1,4 +1,2 @@
--- Initialize database with postgres user then chown it to dawnletter as app user.
-
 CREATE USER dawnletter NOSUPERUSER NOCREATEDB;
 CREATE DATABASE dawnletter WITH OWNER = dawnletter TEMPLATE template0 ENCODING = 'UNICODE';
--- admin/sql/init_test_database.sql
+++ admin/sql/init_test_database.sql
@@ -1,4 +1,2 @@
--- Initialize database with postgres user then chown it to dawnletter as app user.
-
 CREATE USER test_dawnletter NOSUPERUSER NOCREATEDB;
 CREATE DATABASE test_dawnletter WITH OWNER = test_dawnletter TEMPLATE template0 ENCODING = 'UNICODE';
