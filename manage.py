53a05860a69d82bdf6c912b927fc8999cb94a95e
Vriyas Hartama <vriyashartama@gmail.com>
2018-12-22 18:39:48 +0700

    edit manage.py

--- manage.py
+++ manage.py
@@ -10,56 +10,26 @@ import os
 @manager.command
 def test():
     """Runs the unit tests without coverage."""
+    
+    create_test_user()
+    create_test_db()
+
     tests = unittest.TestLoader().discover('tests')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    delete_test_db()
+    delete_test_user()
+    
     if result.wasSuccessful():
         return 0
     else:
         return 1
 
+
 @manager.command
-def cov():
+def cov_local():
 
     print ('Running Test and Coverage Test...')
-
-    def create_test_user():
-        print ('Running Database Setup...')
-
-        db.initialize_db_conn(Config.POSTGRES_ADMIN_URI)
-
-        print ('Creating user and database...')
-        
-        res = db.run_script_init(os.path.join(Config.ADMIN_SQL_DIR, 'init_test_database.sql'))
-        if not res:
-            raise Exception('Falied to create new database and user Exit code: %i' % res)
-
-    def create_test_db():
-        db.initialize_db_conn(Config.SQLALCHEMY_TEST_DATABASE_URI)
-
-        print ('Creating tables...')
-        db.run_script(os.path.join(Config.ADMIN_SQL_DIR, 'init_table.sql'))
-
-        print ('Creating primary keys...')
-        db.run_script(os.path.join(Config.ADMIN_SQL_DIR, 'init_primary_keys.sql'))
-
-        print('Task init_test_db done')
-
-        print ('Running Test...')
-    
-    def delete_test_db():
-        
-        print ('Deleting tables...')
-        db.run_script(os.path.join(Config.ADMIN_SQL_DIR, 'drop_table.sql'))
-
-    def delete_test_user():
-        
-        db.initialize_db_conn(Config.POSTGRES_ADMIN_URI)
-
-        print ('Deleting user and database...')
-
-        res = db.run_script_init(os.path.join(Config.ADMIN_SQL_DIR, 'drop_test_database.sql'))
-        if not res:
-            raise Exception('Falied to delete database and user Exit code: %i' % res)
     
     create_test_user()
     create_test_db()
@@ -77,6 +47,45 @@ def cov():
     print('Coverage Summary:')
     cov.report()
     cov.html_report()
+
+def create_test_user():
+    print ('Running Database Setup...')
+
+    db.initialize_db_conn(Config.POSTGRES_ADMIN_URI)
+
+    print ('Creating user and database...')
+    
+    res = db.run_script_init(os.path.join(Config.ADMIN_SQL_DIR, 'init_test_database.sql'))
+    if not res:
+        raise Exception('Falied to create new database and user Exit code: %i' % res)
+
+def create_test_db():
+    db.initialize_db_conn(Config.SQLALCHEMY_TEST_DATABASE_URI)
+
+    print ('Creating tables...')
+    db.run_script(os.path.join(Config.ADMIN_SQL_DIR, 'init_table.sql'))
+
+    print ('Creating primary keys...')
+    db.run_script(os.path.join(Config.ADMIN_SQL_DIR, 'init_primary_keys.sql'))
+
+    print('Task init_test_db done')
+
+    print ('Running Test...')
+
+def delete_test_db():
+    
+    print ('Deleting tables...')
+    db.run_script(os.path.join(Config.ADMIN_SQL_DIR, 'drop_table.sql'))
+
+def delete_test_user():
+    
+    db.initialize_db_conn(Config.POSTGRES_ADMIN_URI)
+
+    print ('Deleting user and database...')
+
+    res = db.run_script_init(os.path.join(Config.ADMIN_SQL_DIR, 'drop_test_database.sql'))
+    if not res:
+        raise Exception('Falied to delete database and user Exit code: %i' % res)
 
 @manager.command
 def init_user_db():
